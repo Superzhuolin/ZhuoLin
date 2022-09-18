18e58fa976ed4da17b9c32da8f5c9af8873da160
Superzhuolin <1910185040@qq.com>
2022-09-18 16:50:57 +0800

    工具库完成

--- JS工具库/src/axios/index.js
+++ JS工具库/src/axios/index.js
@@ -1,76 +1,75 @@
-
-export function axios({method, url, params, data}){
-    //方法转化大写
+// 使用解构赋值的方式来对参数进行接受收 来直接在函数中使用变量操作变量中的属性
+function axios({method,url,params,data}){
+    // 方法转化大写
     method = method.toUpperCase();
-    //返回值
-    return new Promise((resolve, reject) => {
-        //四步骤
-        //1. 创建对象
+
+    // 返回值
+    return new Promise((resolve,reject)=>{
+        // AJAX四步骤
+        //1.创建对象
         const xhr = new XMLHttpRequest();
-        //2. 初始化
-        //处理 params 对象 a=100&b=200
+        // 2.初始化
+        // 处理params对象  a=100&b=200
         let str = '';
         for(let k in params){
-            str += `${k}=${params[k]}&`;
+            str += `${k}=${params[k]}&`; // str += `a=100&`
         }
-        str = str.slice(0, -1);
-        xhr.open(method, url+'?'+str);
-        //3. 发送
-        if(method === 'POST' || method === 'PUT' || method === 'DELETE'){
-            //Content-type mime类型设置
-            xhr.setRequestHeader('Content-type','application/json');
-            //设置请求体
+        str = str.slice(0,-1);//切取掉最后一位&
+        xhr.open(method,url+"?"+str);
+        // 3.发送
+        if (method === "POST" || method === "PUT" || method ==="DELETE"){
+            // Content-type mime 类型设置
+            xhr.setRequestHeader("Content-type","application/json");
+            // 设置请求体  get一般为空  post/put/delete一般有内容
+            // JSON.stringify 方法将某个对象转换成 JSON 字符串形式。
             xhr.send(JSON.stringify(data));
         }else{
             xhr.send();
         }
-        //设置响应结果的类型为 JSON
-        xhr.responseType = 'json';
-        //4. 处理结果
-        xhr.onreadystatechange = function(){
-            //
-            if(xhr.readyState === 4){
-                //判断响应状态码 2xx
-                if(xhr.status >= 200 && xhr.status < 300){
-                    //成功的状态
+        // 设置响应结果类型为JSON
+        xhr.responseType = "json";
+        // 4.处理结果
+        xhr.onreadystatechange = ()=>{
+            // 
+            if(xhr.readyState  ===4 ){
+                // 判断响应状态码 2xx
+                if(xhr.status >= 200 && xhr.status <300){
+                    // 成功
                     resolve({
-                        status: xhr.status,
-                        message: xhr.statusText,
-                        body: xhr.response
-                    });
+                        status:xhr.status, //成功的状态码
+                        message:xhr.statusText, //响应状态字符串
+                        body:xhr.response //响应体
+                    })
                 }else{
-                    reject(new Error('请求失败, 失败的状态码为' + xhr.status));
+                    // 失败
+                    reject(new Error("请求失败,失败的状态码为"+xhr.status));
                 }
             }
         }
-
-    });
+    })
 }
 
-axios.get = function(url, options){
-    //发送 AJAX 请求 GET
-    let config = Object.assign(options, {method:'GET', url: url});
-   
+axios.get = function(url,options){
+    // 发送AJAX请求GET
+    /* Object.assign将所有可枚举属性的值从一个或多个源对象复制到目标对象，同时返回目标对象 */
+    let config = Object.assign(options, { method: "GET", url});
     return axios(config);
 }
-
-axios.post = function(url, options){
-    //发送 AJAX 请求 GET
-    let config = Object.assign(options, {method:'POST', url: url});
-   
+axios.post = function(url,options){
+    // 发送AJAX请求GET
+    /* Object.assign将所有可枚举属性的值从一个或多个源对象复制到目标对象，同时返回目标对象 */
+    let config = Object.assign(options, { method: "POST", url});
     return axios(config);
 }
-
-axios.put = function(url, options){
-    //发送 AJAX 请求 GET
-    let config = Object.assign(options, {method:'PUT', url: url});
-   
+axios.put = function(url,options){
+    // 发送AJAX请求GET
+    /* Object.assign将所有可枚举属性的值从一个或多个源对象复制到目标对象，同时返回目标对象 */
+    let config = Object.assign(options, { method: "PUT", url});
     return axios(config);
 }
-
-axios.delete = function(url, options){
-    //发送 AJAX 请求 GET
-    let config = Object.assign(options, {method:'delete', url: url});
-   
+axios.delete = function(url,options){
+    // 发送AJAX请求GET
+    /* Object.assign将所有可枚举属性的值从一个或多个源对象复制到目标对象，同时返回目标对象 */
+    let config = Object.assign(options, { method: "DELETE", url});
     return axios(config);
 }
--- JS工具库/src/pub-sub/index.js
+++ JS工具库/src/pub-sub/index.js
@@ -1,6 +1,5 @@
 /* 消息订阅与发布:对代码进行分离实现代码解耦 
     事件总线无法实现单独取消某个绑定*/
-// 订阅频道
 const PubSub = {
     id: 1,//订阅唯一id
     // 频道与回调保存容器
@@ -11,6 +10,7 @@ const PubSub = {
         // }
     }
 }
+// 订阅频道
 PubSub.subscribe = function (channel, callback) {
     // 创建唯一的编号
     let token = "token_" + this.id++;
@@ -22,15 +22,44 @@ PubSub.subscribe = function (channel, callback) {
             [token]: callback,//因为token是变量所以要用[token]表示
         }
     }
+    // 返回频道订阅的ID
+    return token;
 }
 // 发布消息
 PubSub.publish = function (channel, data) {
     // 获取当前频道上的所有回调
     if (this.callbacks[channel]) {
-        // 
         Object.values(this.callbacks[channel]).forEach(callback => {
             callback(data);//执行回调
         });
     }
+}
 
+/*
+4. 取消消息订阅
+  1). 没有传值, flag为undefined
+  2). 传入token字符串
+  3). msgName字符串
+*/
+PubSub.unsubscribe = function(flag){
+    // 如果flag为undefined  则清空所有订阅
+    if(flag === undefined){
+        this.callbacks = {};
+    }else if(typeof flag ==="string"){
+        // 判断是否为token_ 开头
+        if(flag.indexOf("token_") === 0){
+            // 如果是表明是一个订阅ID
+            //Object.values(this.callbacks)获取所有callbacks对象的所有键值
+            // .find(obj=>hasOwnProperty(flag)) 遍历键值对象 并在对象中寻找含有flag的回调对象
+            // callbackObj表示拥有token_Id的对象(pay / cancel)
+            let callbackObj = Object.values(this.callbacks).find(obj => obj.hasOwnProperty(flag));
+            // 判断
+            if(callbackObj){
+                delete callbackObj[flag];
+            }
+        }else{
+            // 表面是一个频道名称
+            delete this.callbacks[flag];//删除对象中的flag对象
+        }
+    }
 }
