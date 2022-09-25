5dc76d2e965791864b6692fd6ace753255c8d9df
Superzhuolin <1910185040@qq.com>
2022-09-25 20:55:19 +0800

    Food类编写完成

--- TS/chapter02/part2/webpack.config.js
+++ TS/chapter02/part2/webpack.config.js
@@ -17,10 +17,10 @@ module.exports = {
         // 打包后文件的文件
         filename: "bundle.js",
 
-        // 告诉webpack不使用箭头
-        environment:{
-            arrowFunction: false,
-            const: false
+        
+        environment: {
+            arrowFunction: false,// 告诉webpack不使用箭头
+            const: false,//不使用const
         }
     },
 
@@ -33,33 +33,33 @@ module.exports = {
                 test: /\.ts$/,
                 // 要使用的loader
                 use: [
-                     // 配置babel
-                     {
-                         // 指定加载器
-                         loader:"babel-loader",
-                         // 设置babel
-                         options: {
-                             // 设置预定义的环境
-                             presets:[
-                                 [
-                                     // 指定环境的插件
-                                     "@babel/preset-env",
-                                     // 配置信息
-                                     {
-                                         // 要兼容的目标浏览器
-                                         targets:{
-                                             "chrome":"58",
-                                             "ie":"11"
-                                         },
-                                         // 指定corejs的版本
-                                         "corejs":"3",
-                                         // 使用corejs的方式 "usage" 表示按需加载
-                                         "useBuiltIns":"usage"
-                                     }
-                                 ]
-                             ]
-                         }
-                     },
+                    // 配置babel
+                    {
+                        // 指定加载器
+                        loader: "babel-loader",
+                        // 设置babel
+                        options: {
+                            // 设置预定义的环境
+                            presets: [
+                                [
+                                    // 指定环境的插件
+                                    "@babel/preset-env",
+                                    // 配置信息
+                                    {
+                                        // 要兼容的目标浏览器
+                                        targets: {
+                                            "chrome": "58",
+                                            "ie": "11"
+                                        },
+                                        // 指定corejs的版本
+                                        "corejs": "3",
+                                        // 使用corejs的方式 "usage" 表示按需加载
+                                        "useBuiltIns": "usage"
+                                    }
+                                ]
+                            ]
+                        }
+                    },
                     'ts-loader'
                 ],
                 // 要排除的文件
@@ -68,8 +68,29 @@ module.exports = {
 
             // 设置less文件的处理
             {
-                test:/\.less$/,
-                use:["less-loader","css-loader","less-loader"],
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+
+                    // 引入postcss
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            browsers: 'last 2 versions'
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
+                    },
+                    "less-loader"
+                ]
             }
         ]
     },
